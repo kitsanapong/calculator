1d7b6c49fe38eecc298021ff87da6240b49c7d19
Kitsanapong Roubgome <kitsanapong.r@gmail.com>
2020-10-16 17:30:04 +0700

    clear history

--- src/App.vue
+++ src/App.vue
@@ -27,6 +27,7 @@
               padding: '10px 20px 20px 20px',
               height: '650px',
               overflow: 'scroll',
+              position: 'relative',
             }">
             <div
               v-for="(item, index) in history"
@@ -45,6 +46,23 @@
               <v-row v-bind:style="{ fontSize: '24px' }">
                 {{item.input}}
               </v-row>
+            </div>
+            <div
+              v-bind:style="{
+                position: 'absolute',
+                bottom: '30px',
+                right: '30px',
+                fontSize: '24px',
+                color: 'white',
+                backgroundColor: 'red',
+                borderRadius: '10px',
+                padding: '5px 15px',
+                boxShadow: '0 3px 1px -2px rgba(0,0,0,.2),0 2px 2px 0 rgba(0,0,0,.14),0 1px 5px 0 rgba(0,0,0,.12)',
+                cursor: 'pointer',
+              }"
+              v-on:click="clearHistory"
+            >
+              Clear
             </div>
           </div>            
           </v-col>
@@ -71,6 +89,9 @@ export default {
     addHistory: function(item) {
       this.history = [item, ...this.history]
     },
+    clearHistory: function() {
+      this.history = []
+    }
   }
 };
 </script>
